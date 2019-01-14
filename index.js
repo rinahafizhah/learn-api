d267def710a54f0086646d730abe6a8aa1308796
Rina Hafizhah U <rinahafizhah@gmail.com>
2019-01-14 17:27:27 +0700

    try show array result of swapi vehicles

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-fetch("https://swapi.co/api/films")
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-  });
+const getDataFilms = () => {
+  fetch("https://swapi.co/api/films")
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    });
+};
+
+getDataFilms();
+
+const getDataSwapi = () => {
+  fetch("https://swapi.co/api/vehicles")
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log(data.results);
+    });
+};
+
+getDataSwapi();
