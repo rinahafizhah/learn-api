ccdbbf77246aa78503b99fb84fff7b0085446214
Rina Hafizhah U <rinahafizhah@gmail.com>
2019-01-15 13:41:28 +0700

    add styles

--- index.js
+++ index.js
@@ -29,9 +29,21 @@ const getDataPeople = () => {
       const array = data.forEach(item => {
         console.log(`Name : ${item.login}URL Profile : ${item.html_url}`);
         document.getElementById("people").innerHTML += `
-          <li>Name : ${item.login}</li>
-          <li>URL Profile : ${item.html_url}`
-        
+         
+          <div class="card" style:"width: 200px; margin: 10px;">
+            <img src=${item.avatar_url} />
+            <div class="card-body">
+              <h5 class="card-title">${item.login}</h5>
+              <p class="card-text"></p>
+              <a href="${
+                item.organizations_url
+              }" class="card-link">Organization Link</a>
+              <a href="${
+                item.html_url
+              }" class="btn btn-primary">Go to Profile</a>
+              
+            </div>
+          </div>`;
       });
     });
 };
